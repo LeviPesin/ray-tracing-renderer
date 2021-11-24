5e7696775f6373e65ba7c37aa0e8df7f39be9a05
LeviPesin <35454228+LeviPesin@users.noreply.github.com>
2021-11-25 02:49:10 +1030

    Update Framebuffer.js

--- src/renderer/Framebuffer.js
+++ src/renderer/Framebuffer.js
@@ -1,42 +1,43 @@
+function bindFramebuffer() {
+  this.gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffer);
+}
+
+function unbindFramebuffer() {
+  this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
+}
+
 export function makeFramebuffer(gl, { color, depth }) {
 
   const framebuffer = gl.createFramebuffer();
+  
+  const thisFramebuffer = {gl, framebuffer};
+  
+  const bind   = bindFramebuffer  .bind(thisFramebuffer);
+  const unbind = unbindFramebuffer.bind(thisFramebuffer);
 
-  function bind() {
-    gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
-  }
+  bind();
 
-  function unbind() {
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-  }
+  const drawBuffers = [];
 
-  function init() {
-    bind();
+  for (let location in color) {
+    location = Number(location);
 
-    const drawBuffers = [];
-
-    for (let location in color) {
-      location = Number(location);
-
-      if (location === undefined) {
-        console.error('invalid location');
-      }
-
-      const tex = color[location];
-      gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0 + location, tex.target, tex.texture, 0);
-      drawBuffers.push(gl.COLOR_ATTACHMENT0 + location);
+    if (Number.isNaN(location)) {
+      console.error('invalid location');
     }
 
-    gl.drawBuffers(drawBuffers);
-
-    if (depth) {
-      gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, depth.target, depth.texture);
-    }
-
-    unbind();
+    const tex = color[location];
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0 + location, tex.target, tex.texture, 0);
+    drawBuffers.push(gl.COLOR_ATTACHMENT0 + location);
   }
 
-  init();
+  gl.drawBuffers(drawBuffers);
+
+  if (depth) {
+    gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, depth.target, depth.texture);
+  }
+
+  unbind();
 
   return {
     color,
